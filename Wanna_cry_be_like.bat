25a4677df79a262fddb05c3b17723f72581110ea
RomsiVmax <romeo.seeger@online.de>
2025-11-30 16:36:43 +0100

    Added new virus
    Added a WannaCry-like acting virus

---
+++ Wanna_cry_be_like.bat
@@ -0,0 +1,7 @@
+@echo off
+color a
+echo You were hacked!
+echo All your files are now encrypted.
+echo Pay 300 US Dollars in Bitcoin to this Bitcoin adress to get your files back: 15NLBE5F6k8SiS8QXPgw4c73C19NFTRfym
+echo You have 3 hours time until your files cant be decrypted again.
+timeout /t 10800
